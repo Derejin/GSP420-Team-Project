b4fd5633223fb3ff4f7846bd41f8c122a6d2fc4c
Khatharr <khatharr@gmail.com>
2016-05-17 16:48:13 -0700

    usage note for Window::update() in header; changed structure of TestWindow.cpp a bit

--- GSP420 Team Project/GSPWindow.h
+++ GSP420 Team Project/GSPWindow.h
@@ -1,12 +1,15 @@
+//GSPWindow class, to set up the game window and provide the window handle (HWND) object
+//for D2D and DirectInput to use.
+
+//created 5/17/2016, 6:00 PM, by Derek Baumgartner
+//modified 5/17, 16:45PST by Richard
+//- added some usage commenting for update()
+
 #pragma once
 #include <Windows.h>
 #include <string>
 #include <assert.h>
 
-//created 5/17/2016, 6:00 PM, by Derek Baumgartner
-
-//GSPWindow class, to set up the game window and provide the window handle (HWND) object
-//for D2D and DirectInput to use.
 
 //This class, with definitions, requires UNICODE to function.
 //For Visual Studio, this means Project Properties->Configuration Properties->General->Character Set
@@ -21,8 +24,9 @@ public:
 	GSPWindow(std::wstring windowTitle, int width, int height);
 	~GSPWindow();
 
-	bool update(); //for reading messages
+	bool update(); //for reading messages - returns false to signal 'quit', otherwise returns true
 	HWND getHandle() const; //for returning handle
+
 	const int WIDTH;
 	const int HEIGHT;
 };
