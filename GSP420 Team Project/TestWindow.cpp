7bd9a7cec1a0e70ccaeed24a005bb1d7b9d29cc0
Khatharr <khatharr@gmail.com>
2016-05-17 17:17:51 -0700

    implemented Graphics class

--- GSP420 Team Project/TestWindow.cpp
+++ GSP420 Team Project/TestWindow.cpp
@@ -1,11 +1,13 @@
 //exists to test GSPWindow functionality.
 //created 5/17/2016, 6:45 PM, by Derek Baumgartner 
-//modified 5/17 4:42PST by Richard
+//modified 5/17 16:42PST by Richard
 //- removed exception handling clause and consolidated into main()
 //- added test of update loop
+//modified 5/17 17:16PST by Richard
+//- added test of Graphics class
 
 #include "GSPWindow.h"
-#include <iostream>
+#include "Graphics.h"
 
 //set screen width and height here
 const int SCREEN_WIDTH = 1080;
@@ -14,9 +16,12 @@ const int SCREEN_HEIGHT = 600;
 //main function - throws error if window fails
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
   GSPWindow gameWindow(L"Testing Window", SCREEN_WIDTH, SCREEN_HEIGHT);
+  Graphics gfx(gameWindow);
 
   while(gameWindow.update()) {
-    //wheeee!
+    gfx.startDraw();
+    //draw calls go here
+    gfx.endDraw();
   }
 
 	return 0;
